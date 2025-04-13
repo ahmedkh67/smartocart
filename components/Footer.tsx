816f0bdad07965991db981e0663f722d2a4d4ee8
ahmedkh67 <al.monster.games@gmail.com>
2025-04-13 08:30:44 +0300

    fgfg

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
-import { footerLinks } from '@/Constants'
+
 
 const Footer = () => {
   return (
--- components/Mapsec.tsx
+++ components/Mapsec.tsx
@@ -6,7 +6,6 @@ import { useState } from 'react'
 const Mapsec = () => {
 
   const [Address , setAddress] = useState("https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3631.259503985478!2d39.60037932820259!3d24.47646387901318!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x15bdbee2739f87f3%3A0xe41454bdfd991dda!2z2KfZhNmF2LPYp9is2K8g2KfZhNiz2KjYudipINin2YTYqtin2LHZitiu2YrYqQ!5e0!3m2!1sar!2ssa!4v1744518029305!5m2!1sar!2ssa") ;
-  const handleChange = (e: { target: { value: React.SetStateAction<string>; }; }) => { setAddress(e.target.value) }
 
   return (
     <>
